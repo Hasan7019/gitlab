bb2b9404af019924849e87840a6b6607e504b0f7
japhethleongyh <96232086+japhethleongyh@users.noreply.github.com>
2023-09-21 12:56:35 +0800

    added other tables to staff.py and some endpoints
    added RO
    added Staff_roles
    added Skills
    added find_by_staff_id
    added find_by_skill

--- staff.py
+++ staff.py
@@ -37,15 +37,60 @@ class Staff(db.Model):
     def json(self):
         return {"staff_id": self.staff_id, "fname": self.fname, "lname":self.lname, "dept": self.dept, "email": self.email, "phone": self.phone, "biz_address": self.biz_address, "sys_role": self.sys_role}
 
+class RO(db.Model):
+    __tablename__ = 'STAFF_REPORTING_OFFICER'
+
+    staff_id = db.Column(db.Integer, primary_key=True)
+    RO_id = db.Column(db.Integer)
+
+    def __init__(self, staff_id, RO_id):
+        self.staff_id = staff_id
+        self.RO_id = RO_id
+
+    def json(self):
+        return {"staff_id": self.staff_id, "RO_id": self.RO_id}
+
+class Staff_roles(db.Model):
+    __tablename__ = 'STAFF_ROLES'
+
+    staff_id = db.Column(db.Integer, primary_key=True)
+    staff_role = db.Column(db.Integer, primary_key=True)
+    role_type = db.Column(db.String(9))
+    sr_status = db.Column(db.String(8))
+
+    def __init__(self, staff_id, staff_role, role_type, sr_status):
+        self.staff_id = staff_id
+        self.staff_role = staff_role
+        self.role_type = role_type
+        self.sr_status = sr_status
+    
+    def json(self):
+        return {"staff_id": self.staff_id, "staff_role":self.staff_role, "role_type":self.role_type, "sr_status":sr_status}
+
+class Skills(db.Model):
+    __tablename__ = 'STAFF_SKILLS'
+
+    staff_id = db.Column(db.Integer, primary_key=True)
+    skill_id = db.Column(db.Integer, primary_key=True)
+    ss_status = db.Column(db.String(8))
+
+    def __init__(self, staff_id, skill_id, ss_status):
+        self.staff_id = staff_id
+        self.skill_id = skill_id
+        self.ss_status = ss_status
+
+    def json(self):
+        return {"staff_id":self.staff_id, "skill_id": self.skill_id, "ss_status": self.ss_status}
+
 @app.route("/staff")
 def get_all():
-    staff = Staff.query.all()
-    if len(stafft):
+    stafflist = Staff.query.all()
+    if len(stafflist):
         return jsonify(
             {
                 "code":200,
                 "data": {
-                    "staff": [book.json() for book in booklist]
+                    "staff": [staff.json() for staff in stafflist]
                 }
             }
         )
@@ -56,7 +101,7 @@ def get_all():
         }
     ), 404
 
-@app.route("/staff/<string:staff_id>")
+@app.route("/staff/<int:staff_id>")
 def find_by_staff_id(staff_id):
     staff = Staff.query.filter_by(staff_id=staff_id).first()
     if staff:
@@ -73,6 +118,29 @@ def find_by_staff_id(staff_id):
         }
     ), 404
     
+@app.route("/staff/<int:skill_id>")
+def find_by_skill(skill_id):
+    staff = Skills.query.filter_by(skill_id=skill_id, ss_status='active')
+    res= []
+    for staffs in staff:
+        res.append(Staff.query.filter_by(staffs.json()["staff_id"]).json())
+    if len(res):
+        return jsonify(
+            {
+                "code":200,
+                "data": {
+                    "staff": res
+                }
+            }
+        )
+    return jsonify(
+        {
+            "code": 404,
+            "message": "There is no staff."
+        }
+    ), 404
+
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
