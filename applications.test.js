d2b08f230ac604db44b6c106f68f0cd405625f98
wooziecodes <84272845+wooziecodes@users.noreply.github.com>
2023-11-06 22:04:57 +0800

    test push

---
+++ applications.test.js
@@ -0,0 +1,31 @@
+// applications.test.js
+const { applyAgain } = require('./applications'); // ensure the path is correct
+
+// Mock fetch before your test cases
+global.fetch = jest.fn(() => Promise.resolve({
+  json: () => Promise.resolve({ success: true }),
+}));
+
+beforeEach(() => {
+  // clear the mock before each test
+  fetch.mockClear();
+});
+
+test('applyAgain makes a PUT request with the correct data', async () => {
+  const role_app_id = '123';
+
+  await applyAgain(role_app_id);
+
+  // check if fetch was called
+  expect(fetch).toHaveBeenCalledTimes(1);
+  expect(fetch).toHaveBeenCalledWith(
+    `http://localhost:5002/role-applications/${role_app_id}`,
+    {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ role_app_status: "applied" }),
+    }
+  );
+});
